25cc29f670cb1de7c13336b4965987a3b7d11dae
BlockBusterBPL <beckettloose@me.com>
2021-11-02 20:27:23 -0400

    Changing IP Again for testing

--- src/main/java/com/beckettloose/swervecontrolthing/NetworkTableSwerveAdapter.java
+++ src/main/java/com/beckettloose/swervecontrolthing/NetworkTableSwerveAdapter.java
@@ -12,19 +12,17 @@ import com.beckettloose.swervecontrolthing.InputHandler;
 public class NetworkTableSwerveAdapter 
 {
     static NetworkTableInstance inst = NetworkTableInstance.getDefault();
+    static NetworkTable table = inst.getTable("JoystickControls");
+
     public static void main( String[] args )
     {
         System.out.println( "Swerve Joystick Controller Starting" );
-        NetworkTable table = inst.getTable("JoystickControls");
-        NetworkTableEntry lxEntry = table.getEntry("0x");
-        NetworkTableEntry lyEntry = table.getEntry("0y");
-        NetworkTableEntry rxEntry = table.getEntry("0rx");
-        NetworkTableEntry ryEntry = table.getEntry("0ry");
-        inst.startClientTeam(3707);
+        inst.startClient("10.0.1.68");
+        //inst.startClientTeam(3707);
         InputHandler.main();
     }
 
     public static void updateTableValueDouble(String entry, double value) {
-        inst.getEntry(entry).forceSetDouble(value);
+        table.getEntry(entry).forceSetDouble(value);
     }
 }
